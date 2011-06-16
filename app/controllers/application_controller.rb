7f9e6742ad4dbde764deb6bce683502af187a66c
Geoff Hayes <hayesgm@gmail.com>
2011-06-16 11:01:55 -0500

    start to get jquery framework together

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  layout 'skimbeat'
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
 end
