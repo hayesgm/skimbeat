cc5e554ab8f99512c6954313a5781e1417ea2dec
Geoff Hayes <hayesgm@gmail.com>
2011-06-16 11:06:39 -0500

    add error logger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,32 @@ class ApplicationController < ActionController::Base
   layout 'skimbeat'
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
+
+  
+  
+  ####################
+  #   Error logging  #
+  ####################
+
+  #TODO: We should make sure this gets logged somewhere
+  #TODO: Add database error logging
+  #TODO: Be careful we don't make any new errors in this
+  def log_error(exception) 
+    super(exception)
+    
+    begin
+      if !exception.instance_of?(ActionController::RoutingError) && !local_request? && !(request.remote_ip =~ /^192[.]168[.].+/)#we're going to skip these for now
+        ErrorMailer.deliver_snapshot(
+          exception,
+          exception.backtrace.nil? ? '' : clean_backtrace(exception).join("<br/>&nbsp;&nbsp;"), 
+          session.inspect, 
+          params.inspect, 
+          request.env.inspect,
+          signed_in? ? current_user : nil)
+      end
+    rescue => e
+      logger.error("Failure to send error e-mail.");
+      logger.error(e)
+    end
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,14 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+ActionMailer::Base.smtp_settings = {
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "skimbeat.com",
+    :authentication => :plain,
+    :user_name => "chattr.llc",
+    :password => "huskyhusky" 
+}
+
